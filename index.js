f76cc4c58d57548d0f1fc530c9b60af1c79abe3d
OlenaYurkina <photoyurkina@gmail.com>
2022-07-09 15:21:00 -0400

    Added API call for daily forecast

--- index.js
+++ index.js
@@ -56,7 +56,8 @@ let currentCalendar = document.querySelector("#date-left");
 currentCalendar.innerHTML = formatCalendar(new Date());
 
 //Display a fake temperature (i.e 17) in Celsius and add a link to convert it to Fahrenheit. When clicking on it, it should convert the temperature to Fahrenheit. When clicking on Celsius, it should convert it back to Celsius.
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Fri", "Sat", "Sun", "Mon", "Tue"];
@@ -83,6 +84,14 @@ function getApiWeather(city) {
   axios.get(apiUrl).then(showTemperature);
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "589d4e7caa7d5afe2a7829afb0f2fcbf";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemperature(response) {
   let temp = Math.round(response.data.main.temp);
   celsiusTemp = response.data.main.temp;
@@ -103,6 +112,8 @@ function showTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 function searchCity(event) {
@@ -183,7 +194,5 @@ let celsiusLink = document.querySelector("#celsius-temp");
 celsiusLink.addEventListener("click", showCelsiusTemp);
 
 let celsiusTemp = null;
-
-displayForecast();
 
 getApiWeather("Lisbon");
